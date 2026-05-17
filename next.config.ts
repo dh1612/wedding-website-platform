0955fca80f8abab41e84b13adaee2c0a99c75803
David <davidhweddings@hotmail.com>
2026-05-17 19:35:01 +0100

    Allow secure remote hero images from intake submissions

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "**"
+      },
+      {
+        protocol: "https",
         hostname: "images.unsplash.com"
       },
       {
